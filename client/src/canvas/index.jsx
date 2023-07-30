b7571152a60c3f3c6468f138ed7a744ca8f21c7b
Levy <levisilvas2018@gmail.com>
2023-07-30 17:28:30 -0300

    criando componentes para a page customizer e para o canvas

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import {Canvas} from '@react-three/fiber';
+import {Environment, Center} from '@react-three/drei';
 
-const Canvas = () => {
+import Shirt from './Shirt';
+import Backdrop from './Backdrop';
+import CameraRig from './CameraRig';
+
+const CanvasTree = () => {
   return (
-    <div>Canvas</div>
+    <Canvas
+      className="w-full max-w-full h-full transition-all ease-in"
+    >
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
+
+      {/* <CameraRig> */}
+        {/* <Backdrop /> */}
+        <Center>
+          <Shirt />
+        </Center>
+      {/* </CameraRig> */}
+    </Canvas>
   )
 }
 
-export default Canvas
+export default CanvasTree
