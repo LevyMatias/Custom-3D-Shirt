e4d936a32e83e29ab00eba99c1b8ce9aa362f782
Levy <levisilvas2018@gmail.com>
2023-08-02 14:32:50 -0300

    Implementa componente Tab para renderizar guias

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { useSnapshot } from 'valtio'
+import state from '../store'
 
-const Tab = () => {
+const Tab = ({tab, isFilterTab, isActiveTab, handleClick}) => {
+  const snap = useSnapshot(state);
+
+  const activeStyles = isFilterTab && isActiveTab 
+  ? { backgroundColor: snap.color, opacity: 0.5 }
+  : { backgroundColor: "transparent", opacity: 1 }
+
   return (
-    <div>Tab</div>
+    <div
+      key={tab.name}
+      className={`tab-btn ${isFilterTab ? 'rounded-full-glassmorphism': 'rounded-4'}`}
+      onClick={handleClick}
+      style={activeStyles}
+    >
+      <img 
+        src={tab.icon}
+        alt={tab.name}
+        className={`${isFilterTab ? 'w-5/6 h-5/6' : 'w-11/12 h-11/12 object-contain'}`}
+      />
+
+    </div>
   )
 }
 
